cea512189029ffba723fac4940676e6742e17bf4
Daniel Mendez <lacasadelduende11@gmail.com>
2025-01-17 15:40:19 -0500

    Prueba técnica

--- invoices/serializers.py
+++ invoices/serializers.py
@@ -3,20 +3,18 @@ from invoices.models import Invoice, InvoiceItem
 
 
 class InvoiceItemSerializer(serializers.ModelSerializer):
-    
     # TODO 4: Descomentar esta línea y resolver el error
-    # unit_price = serializers.ReadOnlyField()
-    
+    unit_price = serializers.ReadOnlyField()
     class Meta:
         model = InvoiceItem
         fields = "__all__"
+
 
 class InvoiceSerializer(serializers.ModelSerializer):
     items = InvoiceItemSerializer(many=True)
     subtotal = serializers.ReadOnlyField()
     total = serializers.ReadOnlyField()
-    
+
     class Meta:
         model = Invoice
         fields = "__all__"
-
--- invoices/views.py
+++ invoices/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, decorators, response
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+
 from invoices.models import Invoice
 from invoices.serializers import InvoiceSerializer
 
@@ -6,14 +9,34 @@ from invoices.serializers import InvoiceSerializer
 class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
-    
+
     @decorators.action(detail=True, methods=['post', 'get'])
     def pay(self, request, pk=None):
         if request.method == 'GET':
             invoice = self.get_object()
+            invoice.paid = True
+            invoice.save()
             return response.Response({'status': 'paid' if invoice.paid else 'unpaid'})
-        
-        # if request.method == 'POST':
-            # TODO 5: Completar la vista para marcar la factura como pagada
-            # Si la factura ya está pagada, devolver un error con status 400
-        
+
+        # TODO 5: Completar la vista para marcar la factura como pagada
+        # Si la factura ya está pagada, devolver un error con status 400
+        if request.method == 'POST':
+            invoice = self.get_object()
+            if invoice.paid:
+                raise NotFound(detail="Esa factura ya estaba paga")
+            else:
+                invoice.paid = True
+                invoice.save()
+                return response.Response({'status': 'paid'})
+
+    @decorators.action(detail=True, methods=['get'])
+    def subtotal(self, request, pk=None):
+        invoice = self.get_object()
+        subtotal = invoice.subtotal()
+        return Response({'subtotal': subtotal})  # Devuelve el resultado    @decorators.action(detail=True, methods=['get'])
+
+    @decorators.action(detail=True, methods=['get'])
+    def total(self, request, pk=None):
+        invoice = self.get_object()
+        total = invoice.total()
+        return Response({'total': total})  # Devuelve el resultado
